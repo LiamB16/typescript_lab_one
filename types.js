dba0d585574596d90a824cbde384a46fe017e65e
unknown <liambryant18@gamil.com>
2021-02-02 18:56:57 +0000

    lab one upload

---
+++ types.js
@@ -0,0 +1,16 @@
+var num1 = 5;
+console.log(num1);
+var flag = false;
+console.log(flag);
+var color = "hello";
+console.log(color);
+var list = ["hi", "hello", "hey"];
+for (var i = 0; i < list.length; i++) {
+    console.log("string " + list[i] + ".");
+}
+var notSure = 5;
+console.log(notSure);
+var listany = [2, true, "test"];
+for (var i = 0; i < list.length; i++) {
+    console.log(listany[i]);
+}
---
+++ types.ts
@@ -0,0 +1,23 @@
+let num1: number = 5;
+console.log(num1);
+
+let flag: boolean = false;
+console.log(flag);
+
+let color: string = "hello";
+console.log(color);
+
+let list: string[] =["hi", "hello", "hey"];
+for(let i = 0; i < list.length; i++)
+{
+  console.log("string " +list[i] + ".");
+}
+
+let notSure: any = 5;
+console.log(notSure);
+
+let listany: any[] = [2, true, "test"];
+for(let i = 0; i < list.length; i++)
+{
+  console.log(listany[i]);
+}
